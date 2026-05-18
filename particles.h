082cdd1514d19de597395efba62809887f35af45
AlexnBray <alexnbray585@gmail.com>
2026-05-18 18:22:24 +1000

    particles update

---
+++ particles.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+struct Particle {
+    sf::Vector2f position;
+    sf::Vector2f velocity;
+    sf::CircleShape shape;
+
+    Particle(float x, float y, float radius, sf::Color color);
+    void draw(sf::RenderWindow& window);
+};
